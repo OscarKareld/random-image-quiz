6792a5a15a32701d91e0ae80567218ef6dedf712
OscarKareld <62706945+OscarKareld@users.noreply.github.com>
2020-12-11 15:47:49 +0100

    ExtAPIHandl en metod påbörjad

--- src/main/java/ExternalAPIHandler.java
+++ src/main/java/ExternalAPIHandler.java
@@ -3,8 +3,19 @@
 public class ExternalAPIHandler {
     private String className = this.getClass().getName();
 
-    public String makeJServiceRequest() {
+    public QuestionCard getQuestionCard(int value) {
         //Skicka en /api/random-request till jService.io
-        return className;
+        QuestionCard questionCard = new QuestionCard();
+        questionCard.setDifficulty(value);
+
+        //Här ska det väl ske ett anrop till jService och Pixabay antar jag
+        return questionCard;
     }
+
+    //Den här metoden finns enbart för att testa ExternalAPIHandler-klassen
+    public static void main(String[] args) {
+
+    }
+
+
 }
