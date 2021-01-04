5c4d0f20e8cf61acda854ab8b6511462f7a03807
rebeckapersson <rebecka_fotboll95@hotmail.com>
2021-01-04 13:29:46 +0100

    result-page

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,95 +1,111 @@
 // känns dumt att ha massa gobala variabler...
-var questions 
+var questions
 var index = -1
 var timerId = 0
-var answers = [] 
+var answers = []
 
-$(document).ready(function() { //denna körs varje gång en sida laddas, det vill vi inte
-    // hämta svårighetsgrad
-    var difficulty = window.location.pathname.replace("/quiz/", "")
-    console.log(difficulty)
-    // hämtar frågorna
-    $.ajax({
-      method: "GET",
-      url: "http://localhost:8080/game/" + difficulty,        
-      headers: {"Accept": "application/json"}  
-    })
+$(document).ready(function () { //denna körs varje gång en sida laddas
+  $('#result-page').hide();
+  // hämta svårighetsgrad
+  var difficulty = window.location.pathname.replace("/quiz/", "")
+  console.log(difficulty)
+  // hämtar frågorna
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:8080/game/" + difficulty,
+    headers: { "Accept": "application/json" }
+  })
     // kör igång spelet
-    .done(function (data) { 
-    console.log(data);
-    questions = data;
-    $('#h2-quiz').text(difficulty + " Quiz");
-    printQuestion(questions[index]);
-    })  
+    .done(function (data) {
+      console.log(data);
+      questions = data;
+      $('#h2-quiz').text(difficulty + " Quiz");
+      printQuestion(questions[index]);
+    })
 });
 
 
-function printQuestion(){
-  if (index == 7){ // när vi kommit till sista frågan avbryter vi
-    showResult()
-    //location.replace("http://localhost:8080/result")
+function printQuestion() {
+  if (index == 2) { // när vi kommit till sista frågan avbryter vi
+    showResult();
   }
 
-  else{
-    index ++
-    $('#h4-quiz').text("Question" + (index+1) + "/8");
+  else {
+    index++
+    $('#h4-quiz').text("Question " + (index + 1) + "/" + questions.length);
     $('.card-text').text(questions[index]['question']);
-    $('#img-clue').attr("src", "/images/cat.jpg"); 
+    $('#img-clue').attr("src", "/images/cat.jpg");
     $('#img-clue').hide();
     // tar bort den gamla timern och skapar en ny
-    clearTimeout(timerId); 
+    clearTimeout(timerId);
     startTimer();
   }
 };
 // när svaret ges körs checkAnswer
 $("#question-form").submit(checkAnswer)
 
-function checkAnswer(event){
-    event.preventDefault() // den gör så att saker funkar, fattar inte riktigt hur dock...
+function checkAnswer(event) {
+  event.preventDefault() // den gör så att saker funkar, fattar inte riktigt hur dock...
 
-    var answer = $("input").val(); // hämtar vårt svar
-    console.log(questions[index]['answer'])
+  var answer = $("input").val(); // hämtar vårt svar
+  console.log(questions[index]['answer'])
 
-    $("#answer").val('') // tömmer input
+  $("#answer").val('') // tömmer input
 
-    if (answer == questions[index]['answer']) {  
-      // sparar resultat
-      var right = { question : questions[index]['question'],
-                    player_answer : "right", 
-                    answer : questions[index]['answer'] }
-      answers.push(right)
-
-      printQuestion() 
-    
+  if (answer == questions[index]['answer']) {
+    // sparar resultat
+    var right = {
+      question: questions[index]['question'],
+      player_answer: 'right',
+      answer: questions[index]['answer']
     }
+    answers.push(right)
+
+    printQuestion()
+
+  }
 };
 
-function startTimer(){
+function startTimer() {
   var timeLeft = 30;
   timerId = setInterval(countdown, 1000);
-  
+
   function countdown() {
     if (timeLeft == -1) {
       // sparar resultat
-      var wrong = { question : questions[index]['question'],
-                    player_answer : "wrong", 
-                    answer : questions[index]['answer'] }
+      var wrong = {
+        question: questions[index]['question'],
+        player_answer: 'wrong',
+        answer: questions[index]['answer']
+      }
       answers.push(wrong)
 
       clearTimeout(timerId);
       printQuestion()
-    } 
+    }
     else {
       if (timeLeft == 15) {
-        $('#img-clue').show(); 
+        $('#img-clue').show();
       }
       $('#timer').text(timeLeft + ' seconds remaining');
       timeLeft--;
     }
-  return timerId
+    return timerId
   }
 }
 
-function showResult(){
-  console.log(answers)
-}
+function showResult() {
+  $('#quiz-page').hide();
+  $('#result-page').show();
+
+  for (i = 0; i < 3; i++) {
+    var item = $(document.createElement('li'));
+    $(item).attr("class", "list-group-item");
+    $(item).html("Question: " + answers[i]['question'] + ": " + answers[i]['answer']);
+    $(item).appendTo(".result-board");
+  };
+};
+
+
+
+
